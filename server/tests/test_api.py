bb9e9247a68ddd3f6dd1ebf9e10f8cf0c2e4d4c9
Chris Wells <iroh@apache.org>
2022-02-11 14:11:46 -0500

    Expanded the test case

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -10,9 +10,13 @@ def test_agendas(client):
     response = client.get("/api/v1/agendas")
 
     assert response.status_code == 200
+    json_response = response.get_json()
+    assert 'board_agenda_2005_07_28.txt' in json_response
 
 
 def test_minutes(client):
     response = client.get("/api/v1/minutes")
 
     assert response.status_code == 200
+    json_response = response.get_json()
+    assert 'board_minutes_1999_04_13.txt' in json_response
