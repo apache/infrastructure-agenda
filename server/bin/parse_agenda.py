38d438a2eba323237e7e41a1126a916dffa8555d
Chris Wells <iroh@apache.org>
2022-03-09 09:19:25 -0500

    Separate agenda into sections

--- server/bin/parse_agenda.py
+++ server/bin/parse_agenda.py
@@ -1,6 +1,7 @@
-#!venv/bin/python
+#!../venv/bin/python
 
 import re
+import datetime
 
 import click
 
@@ -9,24 +10,44 @@ import click
 @click.argument('file')
 @click.option('--section', '-s', default=0, type=int)
 def main(file, section):
-    seperated_file = parse_sections(file)
+    parsed_file = AgendaParser(file)
 
-    print(seperated_file[section])
+    print(parsed_file)
 
 
-def parse_sections(file_name):
-    sections = []
-    with open(file_name, "r") as f:
-        sections.append('')
-        idx = 0
-        for _, line in enumerate(f):
-            if re.search(r'^([ |\d]\d)\.(.*)$', line):
-                sections.append(line)
-                idx += 1
-            else:
-                sections[idx] += line
+class AgendaParser(object):
 
-    return sections
+    def __init__(self, file):
+        self._sections = self._parse_sections(file)
+        self._date = self._parse_meeting_date(self._sections[0]['data'])
+
+    def __repr__(self):
+        return f"<ParsedAgenda: {self._date.date()}>"
+
+    @staticmethod
+    def _parse_meeting_date(section):
+        return datetime.datetime.strptime(section[2], '%B %d, %Y')
+
+    @staticmethod
+    def _parse_sections(file_name):
+        sections = [{'name': 'head', 'data': []}]
+        section_num = 0
+        section_pattern = r'^[ |\d]\d\.\s(.*)$'
+        with open(file_name, "r") as fp:
+
+            for line in fp.readlines():
+                match = re.search(section_pattern, line)
+                if match is not None:
+                    section_num += 1
+                    sections.append({'name': match.group(1).replace(" ", "_").lower(),
+                                     'data': []})
+                else:
+                    line = line.strip()
+                    if line:
+                        sections[section_num]['data'].append(line)
+
+        return sections
+
 
 if __name__ == "__main__":
     main()
