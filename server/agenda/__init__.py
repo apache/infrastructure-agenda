9a6df40598152108e680bb1af1d2f198a64d80c7
Chris Wells <iroh@apache.org>
2022-05-25 16:49:05 -0400

    Temp tweak
    this is hardcoded until I can fix the config loading after tweaking the startup.

--- server/agenda/__init__.py
+++ server/agenda/__init__.py
@@ -3,7 +3,7 @@ import flask
 
 def init_app():
     app = flask.Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object('config.DevelopmentConfig') # TODO: change this to be set by ENV variables
 
     with app.app_context():
         from . import views
