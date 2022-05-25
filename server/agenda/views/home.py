7428f087a89724305c45a8a52fe735d89495f9d6
Chris Wells <iroh@apache.org>
2022-05-25 16:07:09 -0400

    Implementing a basic 'home' view

--- server/agenda/views/home.py
+++ server/agenda/views/home.py
@@ -1,7 +1,16 @@
 import flask
 
+from . import tools
+
 app = flask.current_app
+
+T_HOME = tools.load_template('home.html.ezt')
+
 
 @app.route("/")
 def index():
-    return "HOME"
+    data = {'title': "Home",
+            'body': "This app is full of agendas."
+    }
+
+    return tools.render(T_HOME, data)
