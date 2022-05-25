773785266d81000df4f2a34e40607a40dc3a576e
Chris Wells <iroh@apache.org>
2022-05-25 16:19:31 -0400

    Implementing a basic 'agendas' view

--- server/agenda/views/agenda.py
+++ server/agenda/views/agenda.py
@@ -1,7 +1,17 @@
 import flask
 
+from . import tools
+
 app = flask.current_app
+
+T_HOME = tools.load_template('home.html.ezt')
+
 
 @app.route("/agendas")
 def agendas_index():
-    return "AGENDAS!"
+
+    data = {'title': 'List of Agendas',
+            'body': 'A list of agendas will soon appear here.',
+    }
+
+    return tools.render(T_HOME, data)
