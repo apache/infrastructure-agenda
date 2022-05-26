c27e03f8d21577d83b67c34a767c0e0713974711
Greg Stein <gstein@gmail.com>
2022-05-26 01:43:02 -0400

    nits. comma and eof newline

--- server/agenda/views/agenda.py
+++ server/agenda/views/agenda.py
@@ -15,7 +15,7 @@ def agendas_index():
     items = agendas.get_all()
     formatted_items = [item[0] for item in items]
     data = {'title': 'List of Agendas',
-            'items': formatted_items
+            'items': formatted_items,
     }
 
     return tools.render(T_AGENDAS, data)
--- server/agenda/views/home.py
+++ server/agenda/views/home.py
@@ -10,7 +10,7 @@ T_HOME = tools.load_template('home.html.ezt')
 @app.route("/")
 def index():
     data = {'title': "Home",
-            'body': "This app is full of agendas."
+            'body': "This app is full of agendas.",
     }
 
     return tools.render(T_HOME, data)
