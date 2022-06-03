2eaa09c502d5c2d66e5daffa975ca105244f31d6
Chris Wells <iroh@apache.org>
2022-06-03 13:52:46 -0400

    Updated url mapper and re-added to app

--- server/agenda/__init__.py
+++ server/agenda/__init__.py
@@ -5,6 +5,9 @@ def init_app():
     app = flask.Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.DevelopmentConfig') # TODO: change this to be set by ENV variables
 
+    from . import converters
+    app.url_map.converters['date'] = converters.DateConverter
+
     with app.app_context():
         from . import views
 
--- server/agenda/converters.py
+++ server/agenda/converters.py
@@ -6,13 +6,13 @@ import werkzeug.routing
 class DateConverter(werkzeug.routing.BaseConverter):
     """Extracts a date from the path and validates it"""
 
-    regex = r'\d{4}_\d{2}_\d{2}'
+    regex = r'\d{4}-\d{2}-\d{2}'
 
     def to_python(self, value):
         try:
-            return datetime.datetime.strptime(value, '%Y_%m_%d').date()
+            return datetime.datetime.strptime(value, '%Y-%m-%d').date()
         except ValueError:
             raise werkzeug.routing.ValidationError()
 
     def to_url(self, value):
-        return value.strftime('%Y_%m_%d')
+        return value.strftime('%Y-%m-%d')
