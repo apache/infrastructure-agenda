f6a1f577b1c062c06e890c27735456c9212f663d
Chris Wells <iroh@apache.org>
2022-06-09 15:31:13 -0400

    Reorganized test file and made 100% coverage

--- server/tests/unit/test_svn.py
+++ server/tests/unit/test_svn.py
@@ -1,19 +1,15 @@
 import os
 import datetime
 
+import pytest
+
 from agenda.utils import svn
 import config
 
 CFG = config.TestingConfig
-SVN_DIR = svn.Dir(os.path.join(CFG.DATA_DIR,
-                               'repos',
-                               'foundation_board'),
+SVN_DIR = svn.Dir(os.path.join(CFG.DATA_DIR, 'repos', 'foundation_board'),
                   filter=r'board_agenda_\d{4}_\d{2}_\d{2}\.txt',
                   recurse=True)
-SVN_DIR2 = svn.Dir(os.path.join(CFG.DATA_DIR,
-                                'repos',
-                                'minutes'))
-SVN_FILE = SVN_DIR.file('board_agenda_2015_01_21.txt')
 
 
 def test_dir():
@@ -24,8 +20,17 @@ def test_dir():
     assert str(SVN_DIR) == f"<SVNDir: {SVN_DIR.path}>"
 
 
+def test_dir_with_defaults():
+    svn.Dir(os.path.join(CFG.DATA_DIR, 'repos', 'minutes'))
+
+
 def test_file():
-    assert SVN_FILE.checksum == '74dc12856a54e292025747ec35052b4a84f04291'
-    with open(SVN_FILE.path, 'r') as fp:
-        assert SVN_FILE.contents == fp.read()
-    assert str(SVN_FILE) == '<SVNFile: board_agenda_2015_01_21.txt>'
+    svn_file = SVN_DIR.file('board_agenda_2015_01_21.txt')
+    assert svn_file.checksum == '74dc12856a54e292025747ec35052b4a84f04291'
+    with open(svn_file.path, 'r') as fp:
+        assert svn_file.contents == fp.read()
+    assert str(svn_file) == '<SVNFile: board_agenda_2015_01_21.txt>'
+
+def test_file_missing():
+    with pytest.raises(FileNotFoundError):
+        SVN_DIR.file('this_file_is_not_here.txt')
