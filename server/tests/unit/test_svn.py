78ba5c5574040575b656cda818284e5e14c251ab
Chris Wells <iroh@apache.org>
2022-06-09 15:51:25 -0400

    OF: missing empty line

--- server/tests/unit/test_svn.py
+++ server/tests/unit/test_svn.py
@@ -31,6 +31,7 @@ def test_file():
         assert svn_file.contents == fp.read()
     assert str(svn_file) == '<SVNFile: board_agenda_2015_01_21.txt>'
 
+
 def test_file_missing():
     with pytest.raises(FileNotFoundError):
         SVN_DIR.file('this_file_is_not_here.txt')
