a0a2e466e6c8bb90e5511f0f25b1ffab7612e224
Chris Wells <iroh@apache.org>
2022-06-30 11:53:43 -0400

    Added basic AgendaList class definition and increased cov

--- server/agenda/models/agenda.py
+++ server/agenda/models/agenda.py
@@ -1,5 +1,6 @@
 import os
 import functools
+import pathlib
 
 from ..utils import svn
 from ..parsers import agenda_parser
@@ -39,58 +40,26 @@ class Agenda:
 
 
 class AgendaList:
-    """"""
+    """A class for lists of Agenda objects
 
-    def __init__(self):
-        pass
+    """
 
+    def __init__(self, directory):
+        self._files = [ ]
+        for path in pathlib.Path(directory).rglob(r'board_agenda_????_??_??.txt'):
+            self._files.append(path)
 
+        self.agendas = [ ]
+        for f in self._files:
+            self.agendas.append(Agenda(f))
 
-# import os
-# import datetime
-#
-# #import flask
-#
-# from agenda.utils import svn
-#
-#
-# class Agenda(object):
-#
-#     def __init__(self, data_dir):
-#         self._dir = svn.Dir(os.path.join(data_dir, 'repos', 'foundation_board'),
-#                             filter=r'board_agenda_\d{4}_\d{2}_\d{2}\.txt',
-#                             recurse=True)
-#
-#     def get_all(self, sort=True, desc=False):
-#         agendas = self._dir.files
-#         # # TODO: this only works the first time the agendas index page is displayed
-#         # #       because it changes the underlying objects. So check before changing
-#         # #       or do this differently.
-#         # for agenda in agendas:
-#         #     dt = self._parse_date_from_name(agenda.filename)
-#         #     agenda.name = dt.strftime("%a, %d %b %Y")
-#         #     agenda.url = flask.url_for('find_agenda', meeting_date=dt)
-#         #     agenda.last_changed_date = agenda.last_changed_date.strftime("%c")
-#         #     agenda.minutes = self._minutes_filename(agenda.filename)
-#         #     agenda.minutes_url = flask.url_for('find_minutes', meeting_date=dt)
-#
-#         if sort:
-#             agendas.sort(reverse=desc)
-#
-#         return agendas
-#
-#     def get_by_date(self, date):
-#         filename = f"board_agenda_{date.strftime('%Y_%m_%d')}.txt"
-#
-#         return self._dir.file(filename)
-#
-#     # TODO: move these to shared module so that we can import into other models
-#     @staticmethod
-#     def _parse_date_from_name(fname):
-#         fname_cleaned = fname.rstrip(".txt").split("_")[2:]
-#         return datetime.date(*[int(element) for element in fname_cleaned])
-#
-#     @staticmethod
-#     def _minutes_filename(fname):
-#         return fname.replace('board_agenda', 'board_minutes')
-#
+    def __len__(self):
+        return len(self._files)
+
+    def get_by_date(self, date):
+        results = [agenda for agenda in self.agendas if agenda.date == date]
+
+        return results[0]
+
+    def get_all(self):
+        return self.agendas
--- server/tests/unit/test_agenda_model.py
+++ server/tests/unit/test_agenda_model.py
@@ -13,6 +13,17 @@ def test_agenda_attributes(config):
     assert a.date == datetime.date(2015, 2, 18)
 
 
+def test_agenda_magic(config):
+    a = agenda.Agenda(os.path.join(config.DATA_DIR, 'repos', 'foundation_board', 'board_agenda_2015_01_21.txt'))
+    b = agenda.Agenda(os.path.join(config.DATA_DIR, 'repos', 'foundation_board', 'board_agenda_2015_02_18.txt'))
+
+    assert a.__repr__() == '<Agenda: 2015-01-21>'
+    assert a < b
+    assert a == a
+    assert a != b
+
+
+
 def test_agenda_sorting(config):
     a = agenda.Agenda(os.path.join(config.DATA_DIR, 'repos', 'foundation_board', 'board_agenda_2015_01_21.txt'))
     b = agenda.Agenda(os.path.join(config.DATA_DIR, 'repos', 'foundation_board', 'board_agenda_2015_02_18.txt'))
@@ -21,3 +32,14 @@ def test_agenda_sorting(config):
     tmp_list.sort(reverse=True)
 
     assert tmp_list[0].date == datetime.date(2015, 2, 18)
+
+
+def test_agenda_list(config):
+    agenda_list = agenda.AgendaList(os.path.join(config.DATA_DIR, 'repos', 'foundation_board'))
+    assert len(agenda_list) == 2
+
+    a = agenda_list.get_by_date(datetime.date(2015, 2, 18))
+    assert a.date == datetime.date(2015, 2, 18)
+
+    aa = agenda_list.get_all()
+    assert len(aa) == 2
