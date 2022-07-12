8e80cba9b5dbd6c2b53a94f0ec6bac56aa607086
Chris Wells <iroh@apache.org>
2022-07-12 19:36:37 -0400

    Added all template files to extra_files attribute

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import glob
 
 import dotenv
 dotenv.load_dotenv()
@@ -8,9 +9,9 @@ dotenv.load_dotenv()
 from agenda import init_app
 
 APP_DIR = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_FILES = [f for f in glob.glob(f"{APP_DIR}/agenda/templates/*.ezt")]
 
 app = init_app(APP_DIR)
 
 if __name__ == "__main__":
-    # TODO: create list of all template files to feed below
-    app.run(port=app.config['SERVER_PORT'], extra_files=['./agenda/templates/agenda.html.ezt',])
+    app.run(port=app.config['SERVER_PORT'], extra_files=TEMPLATE_FILES)
